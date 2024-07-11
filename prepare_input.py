c8933a15af624907f3742a787bd92d88ff6cef7e
hp0404 <hryhorii.pavlenko@gmail.com>
2024-07-11 22:28:27 +0200

    update script

--- prepare_input.py
+++ prepare_input.py
@@ -12,21 +12,35 @@ if __name__ == "__main__":
     sentences = pd.read_csv(
         os.path.join(data_raw_path, "240709_candidate_sentences.csv")
     )
-    documents = pd.read_csv(
+    documents = pd.read_csv(os.path.join(data_raw_path, "240709_sentencized.csv"))
+    documents_meta = pd.read_csv(
         os.path.join(data_raw_path, "240709_consolidated_translated_fulltext_only.csv")
     )
+    key = sentences["document_id"] + "_" + sentences["paragraph_id"].astype(str)
+    documents["_key"] = (
+        documents["document_id"] + "_" + documents["paragraph_id"].astype(str)
+    )
 
-    subset = documents.loc[
-        documents["document_id"].isin(sentences["document_id"])
-    ].copy()
-    for record in subset.to_dict(orient="records"):
-        filename = record["document_id"] + ".md"
+    documents = documents.loc[documents["_key"].isin(key)].copy()
+    paragraphs = documents.groupby(["document_id", "paragraph_id"], as_index=False)[
+        "text"
+    ].agg(lambda s: "\n".join(s))
+    paragraphs = pd.merge(
+        paragraphs,
+        documents_meta.loc[:, ["document_id", "TI", "DB", "PY"]],
+        how="left",
+        on="document_id",
+    )
+    print(paragraphs.shape[0])
+    for record in paragraphs.to_dict(orient="records"):
+        filename = record["document_id"] + str(record["paragraph_id"]) + ".md"
         contents = (
-            f"# ID: {record['document_id']}\n"
+            f"# id: {record['document_id']}\n"
+            f"# paragraph: {record['paragraph_id']}\n"
             f"# Title: {record['TI']}\n"
             f"# Database: {record['DB']}\n"
             f"# Year: {record['PY']}\n"
-            f"# Fulltext:\n{record['fulltext_en']}"
+            f"# Fulltext:\n{record['text']}"
         )
         with open(os.path.join(data_input_path, filename), "w", encoding="utf-8") as f:
             f.write(contents)
